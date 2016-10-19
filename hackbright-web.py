03d3bb7b1c35e229ac2b5d6de4e355725a9cf2ae
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-18 17:51:43 -0700

    Add Student Functionality

--- hackbright-web.py
+++ hackbright-web.py
@@ -9,9 +9,33 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    return render_template("student_info.html", first=first, last=last, 
+                            github=github)
+
+
+@app.route("/student-form")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/student-add", methods=['GET'])
+def get_add_form():
+    """Show form for adding students"""
+
+    return render_template("student_add.html")
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+    first_name = request.form.get('firstname')
+    last_name = request.form.get('lastname')
+    github = request.form.get('github')
+    hackbright.make_new_student(first_name, last_name, github)
+    return render_template('add_confirmation.html', first=first_name, last=last_name, github=github)
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
